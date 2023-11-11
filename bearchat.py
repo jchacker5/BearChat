afd9ba981f4b2f06e634d02af13935a80ac23b0f
joe Defendre <jchacker5@gmail.com>
2023-11-11 14:55:50 -0500

    Add OpenAI integration and update Streamlit configuration

--- bearchat.py
+++ bearchat.py
@@ -1,87 +1,77 @@
 import openai
-import os
 import streamlit as st
-from openai import OpenAI
+import requests
+from bs4 import BeautifulSoup
+import pdfkit
 import time
+import os
 from dotenv import load_dotenv
 
-
-# Set the Streamlit page configuration
-st.set_page_config(page_title="🐻💬 BearGPT Chat", layout="wide")
-
-# Streamlit title
-st.title("🐻💬 Welcome to BearGPT Chat")
-
-# Input for OpenAI API Key
-openai_api_key = st.sidebar.text_input("Enter your OpenAI API Key", type="password")
-if not openai_api_key:
-    st.error("Please enter your OpenAI API key to proceed.")
-    st.stop()
-
-# Setting the API key
-
-client = OpenAI(api_key=openai_api_key)
-client.api_key = os.environ.get('OPENAI_API_KEY')
+# Load environment variables and set Streamlit configuration
 load_dotenv()
-# Welcome message
-st.write("Welcome Bears to BearGPT Chat! ...")
+st.set_page_config(page_title="🐻💬 BearChat", layout="wide")
+st.title("🐻💬 Welcome to BearChat")
 
-# Function to create an OpenAI Assistant
+# Initialize OpenAI client with API key
+openai.api_key = os.getenv('OPENAI_API_KEY')
+client = openai
+
+# Assistant configuration
+assistant_id = 'asst_or5rq7uFw9b6Yfcm1MXqOzSE'
+
+# Scrape website and convert text to PDF
+def scrape_website(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup.get_text()
+
+def text_to_pdf(text, filename):
+    config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
+    pdfkit.from_string(text, filename, configuration=config)
+    return filename
+
+def upload_to_openai(filepath):
+    with open(filepath, "rb") as file:
+        response = openai.File.create(file=file, purpose="answers")
+    return response.id
+
+# Create an OpenAI Assistant
 @st.cache(allow_output_mutation=True)
 def create_assistant():
     try:
         return client.beta.assistants.create(
             model="gpt-4-1106-preview",
-            name="BSU Information Assistant",
-            instructions="You are an assistant that provides information ...",
-            tools=[{"type": "code_interpreter"}],
+            name="BearChat Assistant",
+            instructions="Assist students with queries related to Bridgewater State University.",
+            tools=[{"type": "code_interpreter"}, {"type": "web_scraping"}, {"type": "web_search"}]
         )
     except Exception as e:
         st.error(f"Failed to create assistant: {e}")
         st.stop()
 
-# Function to run the assistant and get the response
+assistant = create_assistant()
+
+# Get and display assistant response
 def get_assistant_response(assistant_id, user_message):
     try:
         thread = client.beta.threads.create()
-        message = client.beta.threads.messages.create(
-            thread_id=thread.id,
-            role="user",
-            content=user_message
-        )
-        run = client.beta.threads.runs.create(
-            assistant_id=assistant_id,
-            thread_id=thread.id
-        )
-        
-        # Polling for the assistant's response
-        timeout = 30  # timeout after 30 seconds
+        client.beta.threads.messages.create(thread_id=thread.id, role="user", content=user_message)
+        run = client.beta.threads.runs.create(assistant_id=assistant_id, thread_id=thread.id)
+        timeout = 30  # Timeout after 30 seconds
         start_time = time.time()
         while True:
-            run_status = openai.Run.retrieve(run.id)
-            if run_status['status'] == 'succeeded':
-                break
-            elif run_status['status'] == 'failed':
-                st.error("Assistant run failed.")
-                return []
-            elif time.time() - start_time > timeout:
-                st.error("Timeout occurred while waiting for the assistant's response.")
+            run_status = client.beta.threads.retrieve(run.id)
+            if run_status['status'] == 'succeeded': break
+            elif run_status['status'] == 'failed' or time.time() - start_time > timeout:
+                st.error("Error in processing request.")
                 return []
             time.sleep(1)
-
-        messages = openai.Message.list(thread_id=thread.id)
-        return [msg['content'] for msg in messages.data if msg['role'] == 'assistant']
+        return [msg['content'] for msg in client.beta.threads.messages.list(thread_id=thread.id).data if msg['role'] == 'assistant']
     except Exception as e:
-        st.error(f"Error while getting assistant response: {e}")
+        st.error(f"Error: {e}")
         return []
 
-# Initialize assistant
-assistant = create_assistant()
-
-# User input
-user_input = st.text_input("Ask a question:")
-
-# If there's user input, get and display the response from the assistant
+user_input = st.text_input("Tell me about Bridgewater State University...")
 if user_input and assistant:
     responses = get_assistant_response(assistant.id, user_input)
     for response in responses:
